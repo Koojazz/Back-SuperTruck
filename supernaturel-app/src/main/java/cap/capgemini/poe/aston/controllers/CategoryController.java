647359f609a5d3a0d5ac0e9aa8ace763cd98e484
davidserhani <davidserhani@gmail.com>
2019-02-18 12:48:06 +0100

    fix cors

--- supernaturel-app/src/main/java/cap/capgemini/poe/aston/controllers/CategoryController.java
+++ supernaturel-app/src/main/java/cap/capgemini/poe/aston/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package cap.capgemini.poe.aston.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import cap.capgemini.poe.aston.entities.Category;
 import cap.capgemini.poe.aston.services.ICatergoryService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
