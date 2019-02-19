3aae99ce521e97271975823c1482fe7f047dae98
Akazan83 <n.decoupigny@orange.fr>
2019-02-19 16:59:57 +0100

    add delete method

--- supernaturel-app/src/main/java/cap/capgemini/poe/aston/controllers/ProductController.java
+++ supernaturel-app/src/main/java/cap/capgemini/poe/aston/controllers/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,31 +21,37 @@ import cap.capgemini.poe.aston.services.IProductService;
 @RestController
 @RequestMapping("/api")
 public class ProductController {
-	
+
 	@Autowired
 	private IProductService productService;
-	
+
 	@Autowired
 	private ICatergoryService catergoryService;
-	
+
 	@GetMapping("/products")
 	public List<Product> getAllProducts() {
 		return this.productService.getAllProducts();
 	}
-	
+
 	@GetMapping("/products/{id}")
 	public Product getProductById(@PathVariable Long id) {
 		return this.productService.getProduct(id);
 	}
-		
+
+	@DeleteMapping("/products/{id}")
+	public void delete(@PathVariable Long id) {
+		this.productService.deleteProduct(id);
+	}
+
 	@PostMapping("/products/{categoryId}")
 	public Product createProduct(@PathVariable Long categoryId, @RequestBody Product product) {
-		product.setCategory(catergoryService.getCategory(categoryId));
+		product.setCategory(this.catergoryService.getCategory(categoryId));
 		return this.productService.createProduct(product);
 	}
-	
-	@PutMapping("/products/{id}")
-	public Product update(@PathVariable Long id, @RequestBody Product product){
+
+	@PutMapping("/products/{id}/{categoryId}")
+	public Product update(@PathVariable Long id,@PathVariable Long categoryId, @RequestBody Product product){
+		product.setCategory(this.catergoryService.getCategory(categoryId));
 		return this.productService.editProduct(id, product);
 	}
 }
