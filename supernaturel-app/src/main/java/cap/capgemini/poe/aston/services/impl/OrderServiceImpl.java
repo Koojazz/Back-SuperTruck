4027eefbd871dd7b24bedf0207a3a7771d1a7967
Akazan83 <n.decoupigny@orange.fr>
2019-02-20 17:02:08 +0100

    orders fix

--- supernaturel-app/src/main/java/cap/capgemini/poe/aston/services/impl/OrderServiceImpl.java
+++ supernaturel-app/src/main/java/cap/capgemini/poe/aston/services/impl/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package cap.capgemini.poe.aston.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -11,50 +12,51 @@ import cap.capgemini.poe.aston.services.IOrderService;
 
 @Service
 public class OrderServiceImpl implements IOrderService {
-	
+
+	@Autowired
 	private IOrderRepository orderRepository;
-	
+
 	@Override
 	public Order createOrder(Order order) {
-		return orderRepository.save(order);
+		return this.orderRepository.save(order);
 	}
 
 	@Override
 	public Order getOrder(Long id) {
-		return orderRepository.findById(id).orElse(null);
+		return this.orderRepository.findById(id).orElse(null);
 	}
 
 	@Override
 	public Order editOrder(Long id, Order order) {
-		Order o = orderRepository.findById(id).orElse(null);
+		final Order o = this.orderRepository.findById(id).orElse(null);
 		o.setUser(order.getUser());
 		o.setProducts(order.getProducts());
-		return orderRepository.save(o);
+		return this.orderRepository.save(o);
 	}
 
 	@Override
 	public void deleteOrder(Order order) {
-		orderRepository.delete(order);
+		this.orderRepository.delete(order);
 	}
 
 	@Override
 	public void deleteOrder(Long id) {
-		orderRepository.deleteById(id);
+		this.orderRepository.deleteById(id);
 	}
 
 	@Override
 	public List<Order> getAllOrders(int pageNumber, int pageSize) {
-		return orderRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
+		return this.orderRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
 	}
 
 	@Override
 	public List<Order> getAllOrders() {
-		return orderRepository.findAll();
+		return this.orderRepository.findAll();
 	}
 
 	@Override
 	public long countOrders() {
-		return orderRepository.count();
+		return this.orderRepository.count();
 	}
 
 }
