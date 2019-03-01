0dba41cbd8c83e18295e2b8b14ca7297ff28effa
@cmennevretaston <cmennevretaston@gmail.com>
2019-03-01 14:43:40 +0100

    commit null

--- supernaturel-app/src/main/java/cap/capgemini/poe/aston/entities/User.java
+++ supernaturel-app/src/main/java/cap/capgemini/poe/aston/entities/User.java
@@ -62,11 +62,15 @@ public class User extends  AuditModel{
 	@Size(max = 40)
 	@Email
 	private String email;
+
 	private String phone;
+
 	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER )
 	private List<Order> orders = new ArrayList<>();
+
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+
 	private String picture;
 }
