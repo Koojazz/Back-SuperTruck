9cc7d9d8f04f8c3a1ba8df4bcda073507eaccb6c
Akazan83 <n.decoupigny@orange.fr>
2019-03-04 17:02:08 +0100

    Update OrderController.java

--- supernaturel-app/src/main/java/cap/capgemini/poe/aston/controllers/OrderController.java
+++ supernaturel-app/src/main/java/cap/capgemini/poe/aston/controllers/OrderController.java
@@ -20,28 +20,29 @@ import cap.capgemini.poe.aston.services.IOrderService;
 @RestController
 @RequestMapping("/api")
 public class OrderController {
-	
+
 	@Autowired
 	private IOrderService orderService;
-	
+
 	@GetMapping("/orders")
 	public List<Order> getAllOrders() {
 		return this.orderService.getAllOrders();
 	}
-	
+
 	@GetMapping("/orders/{id}")
 	public Order getOrderById(@PathVariable Long id) {
 		return this.orderService.getOrder(id);
 	}
-	
-	
+
+
 	@PostMapping("/orders")
 	public Order createOrder(@RequestBody RequestWrapper requestWrapper) {
 		return this.orderService.createOrder(requestWrapper.getOrder(), requestWrapper.getIds());
 	}
-	
+
 	@PutMapping("/orders/{id}")
 	public Order update(@PathVariable Long id, @RequestBody Order order){
 		return this.orderService.editOrder(id, order);
 	}
+
 }
