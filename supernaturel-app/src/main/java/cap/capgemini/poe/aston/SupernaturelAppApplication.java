5ae845026a9d818498c4317540dc6122fe069e2f
Akazan83 <n.decoupigny@orange.fr>
2019-03-04 10:20:20 +0100

    Quick fix

--- supernaturel-app/src/main/java/cap/capgemini/poe/aston/SupernaturelAppApplication.java
+++ supernaturel-app/src/main/java/cap/capgemini/poe/aston/SupernaturelAppApplication.java
@@ -47,6 +47,8 @@ public class SupernaturelAppApplication implements CommandLineRunner {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	@Autowired
 	private IContactRepository contactRepository;
 
 	@Autowired
@@ -58,29 +60,25 @@ public class SupernaturelAppApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Set<Role> roles = new HashSet<>();
-		roles.add(roleRepository.save(new Role(null, RoleName.ROLE_ADMIN)));
-		roles.add(roleRepository.save(new Role(null, RoleName.ROLE_USER)));
-			
-		userService.createUser(new User(null, "bob", "square-sponge", passwordEncoder.encode("12345"), "bob@sponge.com", null, null, roles, null));
-		userService.createUser(new User(null, "bruce", "wayne", passwordEncoder.encode("batman"), "bruce@wayne.com", null, null, null, null));
-		userService.createUser(new User(null, "naruto", "uzumaki", passwordEncoder.encode("kyubi"), "naruto@uzumaki.com", null, null, null, null));
-		
-		Category c1 = new Category(null, "sandwich", null);
-		Category c2 = new Category(null, "salade", null);
-		Category c3 = new Category(null, "soupe", null);
-		
-		categoryRepository.save(c1);
-		categoryRepository.save(c2);
-		categoryRepository.save(c3);
-		
-		productRepository.save(new Product(null, "hambourgeois", c1, 8.50, "the best burger", "http://yumm.com/wp-content/uploads/2016/04/1388954443-fergburger-queenstown-new-zealand.jpg", null, null));
-		productRepository.save(new Product(null, "taboulé", c2, 7.50, "the best of lebanon", "https://cdn-elle.ladmedia.fr/var/plain_site/storage/images/elle-a-table/recettes-de-cuisine/taboule-2077780/22032212-3-fre-FR/Taboule.jpg", null, null));
-		productRepository.save(new Product(null, "soupe miso", c3, 6.50, "the best miso", "https://s3-eu-west-1.amazonaws.com/mae-deli/wp-content/uploads/2017/11/06141329/c.jpg", null, null));
-//		productRepository.findAll().forEach(c -> {
-//			System.out.println(c);
-//		});
+
+		final Set<Role> roles = new HashSet<>();
+		roles.add(this.roleRepository.save(new Role(null, RoleName.ROLE_ADMIN)));
+		roles.add(this.roleRepository.save(new Role(null, RoleName.ROLE_USER)));
+
+		this.userService.createUser(new User(null, "bob", "square-sponge", this.passwordEncoder.encode("12345"), "bob@sponge.com", null, null, roles, null));
+		this.userService.createUser(new User(null, "bruce", "wayne", this.passwordEncoder.encode("batman67"), "bruce@wayne.com", null, null, null, null));
+		this.userService.createUser(new User(null, "naruto", "uzumaki", this.passwordEncoder.encode("kyubi67"), "naruto@uzumaki.com", null, null, null, null));
+
+		final Category c1 = new Category(null, "sandwich", null);
+		final Category c2 = new Category(null, "salade", null);
+		final Category c3 = new Category(null, "soupe", null);
+		this.categoryRepository.save(c1);
+		this.categoryRepository.save(c2);
+		this.categoryRepository.save(c3);
+
+		this.productRepository.save(new Product(null, "hambourgeois", c1, 8.50, "the best burger", "http://yumm.com/wp-content/uploads/2016/04/1388954443-fergburger-queenstown-new-zealand.jpg", null, null));
+		this.productRepository.save(new Product(null, "taboulé", c2, 7.50, "the best of lebanon", "https://cdn-elle.ladmedia.fr/var/plain_site/storage/images/elle-a-table/recettes-de-cuisine/taboule-2077780/22032212-3-fre-FR/Taboule.jpg", null, null));
+		this.productRepository.save(new Product(null, "soupe miso", c3, 6.50, "the best miso", "https://s3-eu-west-1.amazonaws.com/mae-deli/wp-content/uploads/2017/11/06141329/c.jpg", null, null));
 
 		this.contactRepository.save(new Contact(Long.valueOf(1),"testBdd@mail.com","0652521415"));
 
